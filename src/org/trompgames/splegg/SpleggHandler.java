f1a4c9c7715d8753ea69d8ff684b81a778e6adca
Thomas <thomas_online@tromp.org>
2015-12-24 00:59:03 -0800

    More updates

--- src/org/trompgames/splegg/SpleggHandler.java
+++ src/org/trompgames/splegg/SpleggHandler.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.trompgames.utils.Updateable;
 
 import com.connorlinfoot.titleapi.TitleAPI;
@@ -47,18 +49,22 @@ public class SpleggHandler extends Updateable{
 		ticks++;
 		if(gameState.equals(GameState.PREGAME) && ticks == 20) preGameUpdate();
 		else if(gameState.equals(GameState.INGAME) && ticks == 20) inGameUpdate();
-		if(ticks >= 20) ticks = 0;
-		for(PlayerData player : players){
-			if(player.isDead()) continue;
-			if(player.getPlayer().getLocation().getBlockY() <= y) killPlayer(player);
+		
+		if(gameState.equals(GameState.INGAME)){
+			for(PlayerData player : players){
+				if(player.isDead()) continue;
+				if(player.getPlayer().getLocation().getBlockY() <= y) killPlayer(player);
+				this.checkWin();
+			}
 		}
 		
+		if(ticks >= 20) ticks = 0;	
 	}
 	
 	public void killPlayer(PlayerData data){
 		Player player = data.getPlayer();		
 		player.setGameMode(GameMode.CREATIVE);
-		Bukkit.broadcastMessage(player.getName() + " Died");
+		Bukkit.broadcastMessage(ChatColor.GOLD + player.getName() + ChatColor.GREEN + " Died");
 		data.setDead(true);
 				
 		for(PlayerData p : players){
@@ -68,14 +74,21 @@ public class SpleggHandler extends Updateable{
 	}
 	
 	public void preGameUpdate(){
-		if(!(players.size() >= minPlayers)) return;		
+		if(!(players.size() >= minPlayers)){
+			if(preGameSeconds != COUNTDOWNSECONDS){
+				preGameSeconds = COUNTDOWNSECONDS;
+				Bukkit.broadcastMessage(ChatColor.GREEN + "Reseting countdown");
+			}
+			return;		
+		}
 		if(preGameSeconds == 30 || preGameSeconds <= 10)
 			sendStartingMessage();			
 		preGameSeconds--;
-		if(preGameSeconds <= 0) startGame();
+		if(preGameSeconds < 0) startGame();
 	}
 	
 	public void sendStartingMessage(){
+		if(preGameSeconds == 0) return;
 		if(preGameSeconds == 1)
 			Bukkit.broadcastMessage(ChatColor.GREEN + "Starting in " + ChatColor.GOLD + preGameSeconds + ChatColor.GREEN + " second!");
 		else
@@ -97,7 +110,7 @@ public class SpleggHandler extends Updateable{
 	
 	public void gameStartingTitle(){
 		for(PlayerData player : players){
-			TitleAPI.sendFullTitle(player.getPlayer(), 0, 10, 10, ChatColor.GREEN + "Stating in " + inGameCountdown + "...", "");
+			TitleAPI.sendFullTitle(player.getPlayer(), 0, 25, 10, ChatColor.GREEN + "Stating in " + inGameCountdown + "...", "");
 		}
 	}
 	
@@ -109,8 +122,10 @@ public class SpleggHandler extends Updateable{
 	
 	public void startGame(){
 		this.gameState = GameState.INGAME;
-		for(PlayerData player : players){
-			player.getPlayer().teleport(mid);
+		for(PlayerData data : players){
+			Player player = data.getPlayer();
+			player.teleport(mid);
+			player.getInventory().addItem(new ItemStack(Material.IRON_SPADE));
 		}
 	}
 	
@@ -119,13 +134,18 @@ public class SpleggHandler extends Updateable{
 		players.add(PlayerData.getPlayerData(player));
 		if(gameState.equals(GameState.PREGAME)) player.teleport(lobbyLocation);
 		else if(gameState.equals(GameState.PREGAME)) player.teleport(mid);
+		if(gameState.equals(GameState.PREGAME))
+			Bukkit.broadcastMessage(ChatColor.GREEN + "➣ " + ChatColor.GOLD + player.getName() + ChatColor.GREEN + " has joined. " + ChatColor.GRAY + "[" + ChatColor.GOLD + players.size() + "/" + maxPlayers + ChatColor.GRAY + "]");
 	}
 	
 	public void playerQuit(Player player){
 		players.remove(PlayerData.getPlayerData(player));
+		if(gameState.equals(GameState.PREGAME)){
+			Bukkit.broadcastMessage(ChatColor.RED + "✘ " + ChatColor.GOLD + player.getName() + ChatColor.GREEN + " has left. " + ChatColor.GRAY + "[" + ChatColor.GOLD + players.size() + "/" + maxPlayers + ChatColor.GRAY + "]");
+		}
 	}
 	
-	public ArrayList<PlayerData> getAllivePlayer(){
+	public ArrayList<PlayerData> getAllivePlayers(){
 		ArrayList<PlayerData> allivePlayers = new ArrayList<PlayerData>();
 		for(PlayerData player : players){
 			if(!player.isDead()) allivePlayers.add(player);
@@ -133,10 +153,24 @@ public class SpleggHandler extends Updateable{
 		return allivePlayers;
 	}
 	
+	public void checkWin(){
+		if(getAllivePlayers().size() == 1){
+			win(getAllivePlayers().get(0));
+		}
+	}
+	
+	public void win(PlayerData player){
+		gameState = GameState.OVER;
+		for(PlayerData data : players){
+			TitleAPI.sendFullTitle(data.getPlayer(), 10, 120, 20, ChatColor.GOLD + "" + ChatColor.BOLD + player.getPlayer().getName() + ChatColor.GREEN + " has won!", "");
+		}
+	}
+	
 	
 	public static enum GameState{
 		PREGAME,
-		INGAME;		
+		INGAME,
+		OVER;		
 	}
 	
 }
